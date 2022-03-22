adb286f978e63893e0ff77b6cccaf6c6c6566038
fatihk29 <fatihkrc7@gmail.com>
2022-03-22 22:58:18 +0300

    first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,17 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import { replaceCamelWithSpaces } from './App';
 import App from './App';
+
+test('button has correct initial color', () => {
+  render(<App />);
+
+  // find an element with a role of button and text of 'Change to blue'
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+
+  // expect the background color to be red
+  expect(colorButton).toHaveStyle({ background: 'red' });
+});
+
+test('button has correct initial text', () => {});
+
+test('button turns blue when clicked', () => {});
